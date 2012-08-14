875f592c39e4ea60eea6ca800bb8cd5f0acd48fc
Dan Fennell <dan.fennell.jr@gmail.com>
2012-08-14 17:39:43 -0400

    add amqp logger via winston framework

--- test/config/config.js
+++ test/config/config.js
@@ -1,25 +1,75 @@
 var config = exports;
 
 config.gateway = {
-    enabled : false
+    enabled : true
     , name : 'gateway'
     , options : { host: 'arch-lb-01.dev.purchasingpwr.com'
-                , port: 5672
-                , login: 'http-router'
-                , password: 'http-router'
-                , vhost: 'communication-external'
-                }
+        , port: 5672
+        , login: 'svc-router'
+        , password: 'svc-router'
+        , vhost: 'communication-external'
+        , heartbeat : 10
+    }
     , implOptions : { defaultExchangeName: 'svc.gateway'
-                    , reconnect: true
-                    , reconnectBackoffStrategy: 'linear'
-                    , reconnectBackoffTime: 1000
-                    , heartbeat : 5
-                    , heartbeatIntervalSec : 60
-                    }
+        , reconnect: true
+        , reconnectBackoffStrategy: 'linear'
+        , reconnectBackoffTime: 1000
+    }
     , exchangeConnectParams: {
-         type: 'direct'
+        type: 'direct'
         , durable: true
         , passive: false
         , autoDelete: false
+    }
+};
+
+
+config.alerts = {
+    enabled : true
+    , name : 'alert'
+    , usedForLogging : true
+    , loggerType : winston.transports.AmqpLogger
+    , options : { host: 'arch-lb-01.dev.purchasingpwr.com'
+        , port: 5672
+        , login: 'alert-client'
+        , password: 'alert-client'
+        , vhost: 'control-channel'
+        , heartbeat : 55
+    }
+    , implOptions : { defaultExchangeName: 'control.gateway'
+        , reconnect: true
+        , reconnectBackoffStrategy: 'linear'
+        , reconnectBackoffTime: 1000
+    }
+    , exchangeConnectParams: {
+        type: 'topic'
+        , durable: true
+        , passive: false
+        , autoDelete: false
+    }
+    , queues: { alertsTrace : { enabled : true
+        , name : 'alerts-trace'
+        , options: {passive: false, durable: false, exclusive: false, autoDelete: false}
+        , routingKeys: ['*.trace.log'] }
+        , alertsDebug : { enabled : true
+            , name : 'alerts-debug'
+            , options: {passive: false, durable: false, exclusive: false, autodDlete: false}
+            , routingKeys: ['*.debug.log']}
+        , alertsInfo : { enabled : true
+            , name : 'alerts-info'
+            , options: {passive: false, durable: false, exclusive: false, autoDelete: false}
+            , routingKeys: ['*.info.log']}
+        , alertsWarn : { enabled : true
+            , name : 'alerts-warn'
+            , options: {passive: false, durable: true, exclusive: false, autoDelete: false}
+            , routingKeys: ['*.warn.log']}
+        , alertsError : { enabled : true
+            , name : 'alerts-error'
+            , options: {passive: false, durable: true, exclusive: false, autoDelete: false}
+            , routingKeys: ['*.error.log']}
+        , altersFatal : { enabled : true
+            , name : 'alerts-fatal'
+            , options: {passive: false, durable: true, exclusive: false, autoDelete: false}
+            , routingKeys: ['*.fatal.log']}
     }
 };
